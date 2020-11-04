75d9f5a44e416e5bf175c08e60b1c72dc144792b
time <3486197615@qq.com>
2020-11-04 10:59:37 +0800

    错误自定义

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  
+  <title>404</title>
+  <link rel="stylesheet" href="/static/error/style.css">
+  <!-- 小头 -->
+  <!-- <link rel="icon" href="/statis/layui/images/face/0.gif"> -->
+</head>
+<body>
+
+    <section class="wrapper">
+
+        <div class="container">
+
+            <div id="scene" class="scene" data-hover-only="false" style="transform: translate3d(0px, 0px, 0px) rotate(0.0001deg); transform-style: preserve-3d; backface-visibility: hidden; pointer-events: none;">
+
+
+                <div class="circle" data-depth="1.2" style="transform: translate3d(0px, 0px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: relative; display: block; left: 0px; top: 0px;"></div>
+
+                <div class="one" data-depth="0.9" style="transform: translate3d(0px, 0px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block; left: 0px; top: 0px;">
+                    <div class="content">
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                    </div>
+                </div>
+
+                <div class="two" data-depth="0.60" style="transform: translate3d(0px, 0px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block; left: 0px; top: 0px;">
+                    <div class="content">
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                    </div>
+                </div>
+
+                <div class="three" data-depth="0.40" style="transform: translate3d(0px, 0px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block; left: 0px; top: 0px;">
+                    <div class="content">
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                    </div>
+                </div>
+
+                <p class="p404" data-depth="0.50" style="transform: translate3d(0px, 0px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block; left: 0px; top: 0px;">404</p>
+                <p class="p404" data-depth="0.10" style="transform: translate3d(0px, 0px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block; left: 0px; top: 0px;">404</p>
+
+            </div>
+
+            <div class="text">
+                <article>
+                    <p>您可能迷路了!!!</p>
+                    <button onclick="history.back()">返回</button>
+                </article>
+            </div>
+
+        </div>
+    </section>
+	
+	<script src="/static/error/parallax.min.js"></script>
+	<script src="/static/error/jquery.min.js"></script>
+	<script src="/static/error/script.js"></script>
+
+
+</body></html>
