fe256b57593be7142976c457d77f59d95cf472c2
time <3486197615@qq.com>
2020-11-16 08:47:54 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -36,37 +36,38 @@ Route::domain('admin.yqc.ink')->group(function(){
     Route::middleware('Admin')->group(function(){
         // 后台首页
         Route::view('/', 'admin.adminmain.index');
+        // RBAC 后台管理员
         Route::prefix('admin')->group(function(){
-            // RBAC 后台管理员
+            // 管理员添加
             Route::get('/Adminuser',[AdministratorController::class,'Adminuser']);
-            // RBAC 后台管理员添加
+            // 管理员添加   #没加验证
             Route::any('/InsertAdminuser',[AdministratorController::class,'InsertAdminuser']);
-            // RBAC 后台管理员列表
+            // 管理员列表
             Route::get('/ListAdminuser',[AdministratorController::class,'ListAdminuser']);
-            // RBAC 后台角色
+            // 后台角色添加
             Route::get('/Adminrole',[AdministratorController::class,'Adminrole']);
             // RBAC 后台角色添加
             Route::post('/InsertAdminrole',[AdministratorController::class,'InsertAdminrole']);
             // RBAC 后台角色列表
             Route::get('/ListAdminrole',[AdministratorController::class,'ListAdminrole']);
-            // RBAC 后台权限
+            // RBAC 后台菜单表单
             Route::get('/Adminmenu',[AdministratorController::class,'Adminmenu']);
-            // RBAC 后台权限添加
+            // RBAC 后台菜单添加
             Route::post('/InsertAdminmenu',[AdministratorController::class,'InsertAdminmenu']);
-            // RBAC 后台权限列表
+            // RBAC 后台菜单列表
             Route::get('/ListAdminmenu',[AdministratorController::class,'ListAdminmenu']);
-            // 优惠模块
-            Route::prefix('coupon')->group(function(){
-                Route::any('create', [Couponcontroller::class, 'create'])->name('coupon.create');  //优惠添加
-                Route::any('store', [Couponcontroller::class, 'store'])->name('coupon.store');  //优惠执行添加
-                Route::any('/', [Couponcontroller::class, 'index'])->name('coupon.index');         //优惠展示
-                Route::any('del', [Couponcontroller::class, 'destroy'])->name('coupon.del');       //优惠删除
-                Route::any('update', [Couponcontroller::class, 'update'])->name('coupon.update');  //优惠修改
-            });
-            // 运营模块
-            Route::prefix('operate')->group(function(){
-                Route::any('/',[OperateController::class , 'operate'])->name('operate');
-            });
+        });
+        // 优惠模块
+        Route::prefix('coupon')->group(function(){
+            Route::any('create', [Couponcontroller::class, 'create'])->name('coupon.create');  //优惠添加
+            Route::any('store', [Couponcontroller::class, 'store'])->name('coupon.store');  //优惠执行添加
+            Route::any('/', [Couponcontroller::class, 'index'])->name('coupon.index');         //优惠展示
+            Route::any('del', [Couponcontroller::class, 'destroy'])->name('coupon.del');       //优惠删除
+            Route::any('update', [Couponcontroller::class, 'update'])->name('coupon.update');  //优惠修改
+        });
+        // 运营模块
+        Route::prefix('operate')->group(function(){
+            Route::any('/',[OperateController::class , 'operate'])->name('operate');
         });
         //品牌
         Route::prefix('brand')->group(function(){
